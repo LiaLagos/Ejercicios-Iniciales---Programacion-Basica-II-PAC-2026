90fd168665857c06b2f96c30e59adc4a9d6f00d3
LiaLagos <lialagos177@gmail.com>
2026-06-11 17:52:47 -0600

    funciones par

--- 06_funciones/FuncionEsPar.cpp
+++ 06_funciones/FuncionEsPar.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+using namespace std;
 // Función para determinar si un número es par o impar
 int esPar(int n) { 
     return n % 2 == 0; // % divide entre 2 y devuelve el resto, si el resto es 0, entonces es par
@@ -6,13 +7,13 @@ int esPar(int n) {
 
 int main() {
     int numero;
-    std::cout << "Ingrese un numero: ";
-    if (!(std::cin >> numero)) return 1;
+    cout << "Ingrese un numero: ";
+    cin >> numero; // solicita al usuario que ingrese un número
 
     if (esPar(numero))
-        std::cout << "El numero " << numero << " es par." << std::endl;
+        cout << "El numero " << numero << " es par." << endl; //
     else
-        std::cout << "El numero " << numero << " es impar." << std::endl; 
+        cout << "El numero " << numero << " es impar." << endl; 
 
     return 0;
 }
